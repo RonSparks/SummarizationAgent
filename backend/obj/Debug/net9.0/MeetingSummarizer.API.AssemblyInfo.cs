e293f4dfe3dad0cc981a716f7ca7f152bc5ad89e
Ron Sparks <ron.sparks@gmail.com>
2025-07-10 20:24:00 -0400

    Added timer

--- backend/obj/Debug/net9.0/MeetingSummarizer.API.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/MeetingSummarizer.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingSummarizer.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ffdcb17f41a521ab6f1c3351a723eab728aaf9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingSummarizer.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingSummarizer.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
